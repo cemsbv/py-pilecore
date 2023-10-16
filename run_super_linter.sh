df3cd3fb8c7783cf8c3974470080e8d936404b59
Thijs Lukkezen <t.lukkezen@cemsbv.io>
2023-10-16 13:41:51 +0200

    lint: Add run_super_linters.sh bash script

---
+++ run_super_linter.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+docker run \
+--env VALIDATE_ALL_CODEBASE=false \
+--env RUN_LOCAL=true \
+--env VALIDATE_JSCPD=false \
+--env VALIDATE_CSS=false \
+--env VALIDATE_BASH=false \
+--env VALIDATE_YAML=false \
+--env VALIDATE_PYTHON_PYLINT=false \
+--env VALIDATE_NATURAL_LANGUAGE=false \
+--env VALIDATE_MARKDOWN=false \
+--env LINTER_RULES_PATH=/ \
+--env PYTHON_BLACK_CONFIG_FILE=pyproject.toml \
+--env PYTHON_ISORT_CONFIG_FILE=pyproject.toml \
+--env PYTHON_MYPY_CONFIG_FILE=pyproject.toml \
+--env PYTHON_FLAKE8_CONFIG_FILE=.flake8 \
+-v $(pwd):/tmp/lint github/super-linter:v4
