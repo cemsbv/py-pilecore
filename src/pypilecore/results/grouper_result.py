24abd8b7c28218f146af749d3ddf897ecaa21b33
Robin Wimmers <r.wimmers@cemsbv.io>
2023-10-23 10:27:35 +0200

    feat(grouper): add GrouperResults dataclass

---
+++ src/pypilecore/results/grouper_result.py
@@ -0,0 +1,516 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+from typing import Any, List, Tuple
+
+import natsort
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+from shapely import MultiPoint
+
+
+@dataclass(frozen=True)
+class SingleClusterData:
+    """
+    *Not meant to be instantiated by the user.*
+    """
+
+    characteristic_bearing_capacity: List[float]
+    design_bearing_capacity: List[float]
+    design_negative_friction: List[float]
+    group_centre_to_centre_validation: List[bool]
+    group_centre_to_centre_validation_15: List[bool]
+    group_centre_to_centre_validation_20: List[bool]
+    group_centre_to_centre_validation_25: List[bool]
+    mean_calculated_bearing_capacity: List[float]
+    min_calculated_bearing_capacity: List[float]
+    net_design_bearing_capacity: List[float]
+    nominal_cpt: List[str]
+    pile_tip_level: List[float]
+    variation_coefficient: List[float]
+    xi_factor: List[str]
+    xi_values: List[float]
+
+    def __post_init__(self) -> None:
+        raw_lengths = [len(values) for values in self.__dict__.values()]
+        if len(list(set(raw_lengths))) > 1:
+            raise ValueError("All values in this dataclass must have the same length.")
+
+    @property
+    def dataframe(self) -> pd.DataFrame:
+        return pd.DataFrame(self.__dict__)
+
+    def plot_variation_coefficient(
+        self,
+        axes: plt.Axes,
+    ) -> None:
+        """
+        Plot the bearing capacity and variation coefficient in a subplot
+
+        Parameters
+        ----------
+        axes:
+            `plt.Axes` object where the data can be plotted on.
+        """
+        # create variation coefficient plot
+        axes.plot(self.variation_coefficient, self.pile_tip_level, "o-")
+        axes.axvline(x=0.12, color="black", linestyle="--")
+        axes.grid()
+        axes.set_xlabel("Variation coefficient [-]")
+
+    def plot_bearing_capacity(
+        self,
+        axes: plt.Axes,
+        pile_load_uls: float,
+    ) -> None:
+        """
+        Plot the bearing capacity and variation coefficient in a subplot
+
+        Notes
+        ------
+        For the `Net bearing capacity` subplot there are two colors plotted:
+         - orange:  conservative bearing capacity
+         - blue:    net bearing capacity
+
+        Parameters
+        ----------
+        axes:
+            `plt.Axes` object where the data can be plotted on.
+        pile_load_uls:
+            ULS load in kN.
+        """
+        # add net bearing capacity to plot
+        axes.scatter(
+            self.net_design_bearing_capacity,
+            self.pile_tip_level,
+            color=list(
+                map(
+                    lambda x: "tab:blue" if x <= 0.12 else "tab:orange",
+                    self.variation_coefficient,
+                )
+            ),
+        )
+        axes.axvline(x=pile_load_uls, color="black", linestyle="--")
+        axes.grid()
+        axes.set_xlabel("Net bearing capacity [kN]")
+
+    def plot_group_centre_to_centre_validation(
+        self,
+        axes: plt.Axes,
+    ) -> None:
+        """
+        Plot the spacing checks in a subplot
+
+        Notes
+        ------
+        For the `spacing` subplot there are two colors plotted:
+         - red:     invalid spacing
+         - green:   valid spacing
+
+        Parameters
+        ----------
+        axes:
+            `plt.Axes` object where the data can be plotted on.
+        """
+        axes.scatter(
+            [0] * len(self.pile_tip_level),
+            self.pile_tip_level,
+            marker="o",
+            color=list(
+                map(
+                    lambda x: "tab:green" if x else "tab:red",
+                    self.group_centre_to_centre_validation_25,
+                )
+            ),
+        )
+        axes.scatter(
+            [1] * len(self.pile_tip_level),
+            self.pile_tip_level,
+            marker="s",
+            color=list(
+                map(
+                    lambda x: "tab:green" if x else "tab:red",
+                    self.group_centre_to_centre_validation_20,
+                )
+            ),
+        )
+        axes.scatter(
+            [2] * len(self.pile_tip_level),
+            self.pile_tip_level,
+            marker="D",
+            color=list(
+                map(
+                    lambda x: "tab:green" if x else "tab:red",
+                    self.group_centre_to_centre_validation_15,
+                )
+            ),
+        )
+        axes.grid()
+        axes.set_xticks([0, 1, 2], ["25", "20", "15"])
+        axes.set_xlabel("CPT ctc [m]")
+
+    def plot_xi(
+        self,
+        axes: plt.Axes,
+    ) -> None:
+        """
+        Plot the xi factor in a subplot
+
+        Notes
+        ------
+        For the `xi factor` subplot there are two colors plotted:
+         - olive:   xi3
+         - cyan:    xi4
+
+        Parameters
+        ----------
+        axes:
+            `plt.Axes` object where the data can be plotted on.
+        """
+
+        axes.scatter(
+            self.xi_values,
+            self.pile_tip_level,
+            color=list(
+                map(
+                    lambda i: "tab:cyan" if i == "\u03BE4" else "tab:olive",
+                    self.xi_factor,
+                )
+            ),
+        )
+        axes.grid()
+        axes.set_xlabel("xi value [-]")
+
+
+@dataclass(frozen=True)
+class SingleClusterResult:
+    """
+    *Not meant to be instantiated by the user.*
+    """
+
+    cpt_names: List[str]
+    coordinates: List[float]
+    pile_load_uls: float
+    maximum_pile_level: float
+    minimum_pile_level: float
+    number_of_consecutive_pile_levels: int
+    pile_load_check: bool
+    spatial_check: bool
+    variation_check: bool
+    centre_to_centre_check: bool
+    data: SingleClusterData
+
+    @classmethod
+    def from_api_response(
+        cls, response_dict: dict, pile_load_uls: float
+    ) -> "SingleClusterResult":
+        try:
+            return cls(
+                cpt_names=response_dict["names"],
+                coordinates=response_dict["coordinates"],
+                pile_load_uls=pile_load_uls,
+                maximum_pile_level=response_dict["maximum_pile_level"],
+                minimum_pile_level=response_dict["minimum_pile_level"],
+                number_of_consecutive_pile_levels=response_dict[
+                    "number_of_consecutive_pile_levels"
+                ],
+                pile_load_check=response_dict["pile_load_check"],
+                spatial_check=response_dict["spatial_check"],
+                variation_check=response_dict["variation_check"],
+                centre_to_centre_check=response_dict["centre_to_centre_check"],
+                data=SingleClusterData(**response_dict["table"]),
+            )
+        except KeyError as e:
+            raise KeyError(
+                "Response dictionary is missing an expected key.\n" rf"Traceback: {e}"
+            )
+        except ValueError as e:
+            raise ValueError(
+                f"Could not create `SingleClusterResult` class with the following cpts: {response_dict.get('names')} \n"
+                rf"Traceback: {e}"
+            )
+
+    def map(
+        self,
+        figsize: Tuple[int, int] | None = None,
+        **kwargs: Any,
+    ) -> plt.Figure:
+        """
+        Plot a map of the cpt locations
+
+        Parameters
+        ----------
+        figsize:
+            Size of the activate figure, as the `plt.figure()` argument.
+        **kwargs:
+            All additional keyword arguments are passed to the `pyplot.subplots()` call.
+
+        Returns
+        -------
+        axes:
+            The `Axes` object where the data was plotted on.
+        """
+
+        # Create axes objects if not provided
+        kwargs_subplot = {
+            "figsize": figsize,
+            "tight_layout": True,
+        }
+
+        kwargs_subplot.update(kwargs)
+
+        fig, axes = plt.subplots(
+            **kwargs_subplot,
+        )
+
+        # plot cpt
+        xy: List[List[List[float]]] = list(*zip(*self.coordinates))
+        axes = axes.scatter(x=xy[0], y=xy[1], color="grey")
+
+        # add labels (cpt names) to map
+        for x, y, label in zip(xy[0], xy[1], self.cpt_names):
+            axes.annotate(label, xy=(x, y), xytext=(3, 3), textcoords="offset points")
+        axes.ticklabel_format(useOffset=False, style="plain")
+        return fig
+
+    def plot(
+        self,
+        figsize: Tuple[int, int] | None = None,
+        **kwargs: Any,
+    ) -> plt.Figure:
+        """
+        Plot contains the:
+            - bearing capacity
+            - variation coefficient
+            - xi factor
+            - centre to centre validation
+
+        Notes
+        ------
+        For the `Net bearing capacity` subplot there are two colors plotted:
+         - orange:  conservative bearing capacity
+         - blue:    net bearing capacity
+
+        For the `xi factor` subplot there are two colors plotted:
+         - olive:   xi3
+         - cyan:    xi4
+
+        For the `spacing` subplot there are two colors plotted:
+         - red:     invalid spacing
+         - green:   valid spacing
+
+        Parameters
+        ----------
+        figsize:
+            Size of the activate figure, as the `plt.figure()` argument.
+        **kwargs:
+            All additional keyword arguments are passed to the `pyplot.subplots()` call.
+
+        Returns
+        -------
+        figure:
+            `plt.Figure` object.
+        """
+
+        kwargs_subplot = {
+            "sharey": "row",
+            "sharex": "col",
+            "figsize": figsize,
+        }
+
+        kwargs_subplot.update(kwargs)
+
+        fig, axes = plt.subplots(
+            1,
+            4,
+            **kwargs_subplot,
+        )
+        # add plot variation coefficient
+        self.data.plot_variation_coefficient(axes[0])
+
+        # add plot bearing capacity
+        self.data.plot_bearing_capacity(axes[1], self.pile_load_uls)
+
+        # add xi table
+        self.data.plot_xi(axes[2])
+
+        # add centre to centre
+        self.data.plot_group_centre_to_centre_validation(axes[3])
+
+        return fig
+
+
+@dataclass(frozen=True)
+class GrouperResults:
+    clusters: List[SingleClusterResult]
+
+    @classmethod
+    def from_api_response(
+        cls, response_dict: dict, pile_load_uls: float
+    ) -> "GrouperResults":
+        """
+        Stores the response of the PileCore endpoint
+        "/grouper/group_cpts"
+
+        Parameters
+        ----------
+        response_dict:
+           The resulting response of a call to `get_groups_api_result()`
+        pile_load_uls:
+            ULS load in kN. Used to determine if a grouping configuration is valid.
+        """
+        results = [
+            SingleClusterResult.from_api_response(item, pile_load_uls)
+            for item in response_dict["sub_groups"]
+        ]
+        return cls(clusters=results)
+
+    def map(
+        self,
+        distance: float = 25.0,
+        add_tags: bool = True,
+        figsize: Tuple[int, int] | None = None,
+        **kwargs: Any,
+    ) -> plt.Figure:
+        """
+        Plot a map of the valid subgroups.
+        Plot contains the:
+            - convex_hull of the subgroup with buffer distance
+            - All CPT's with tag
+
+        Parameters
+        ----------
+        distance : float, optional
+            Default is 25.
+            The buffer around the convex_hull of the subgroup
+        add_tags : bool, optional
+            default is True
+            Show the CTP names as tags on the map
+        figsize:
+            Size of the activate figure, as the `plt.figure()` argument.
+        **kwargs:
+            All additional keyword arguments are passed to the `pyplot.subplots()` call.
+
+        Returns
+        -------
+        figure:
+            The `Figure` object where the data was plotted on.
+        """
+
+        kwargs_subplot = {
+            "figsize": figsize,
+            "tight_layout": True,
+        }
+
+        kwargs_subplot.update(kwargs)
+
+        figure, axes = plt.subplots(
+            **kwargs_subplot,
+        )
+
+        for group_id, cluster in enumerate(self.clusters):
+            # add cpts to plot
+            xy = list(zip(*cluster.coordinates))
+            axes.scatter(xy[0], xy[1], color="grey")
+
+            if add_tags:
+                for x, y, label in zip(xy[0], xy[1], cluster.cpt_names):
+                    axes.annotate(
+                        label, xy=(x, y), xytext=(3, 3), textcoords="offset points"
+                    )
+
+            # add group convex hull
+            polygon = (
+                MultiPoint(cluster.coordinates)
+                .convex_hull.buffer(distance=distance)
+                .exterior
+            )
+            axes.plot(polygon.xy[0], polygon.xy[1], label=f"Group {group_id}")
+
+        axes.legend(bbox_to_anchor=(1, 1), loc="upper left")
+        axes.ticklabel_format(useOffset=False, style="plain")
+
+        return figure
+
+    def plot(
+        self,
+        figsize: Tuple[int, int] | None = None,
+        **kwargs: Any,
+    ) -> plt.Figure:
+        """
+        Plot a summary of the valid subgroups.
+        Plot contains the:
+            - cpts within a subgroup
+            - valid depth of the subgroup
+
+        Parameters
+        ----------
+        figsize:
+            Size of the activate figure, as the `plt.figure()` argument.
+        **kwargs:
+            All additional keyword arguments are passed to the `pyplot.subplots()` call.
+
+        Returns
+        -------
+        figure:
+            the `plt.Figure` object.
+        """
+        kwargs_subplot = {
+            "sharex": "row",
+            "figsize": figsize,
+            "tight_layout": True,
+        }
+
+        kwargs_subplot.update(kwargs)
+
+        figure, axes = plt.subplots(
+            1,
+            2,
+            **kwargs_subplot,
+        )
+
+        # place holds needed to sort for plot
+        group_id_list_sort = []
+        cpt_names_list: List[str] = []
+        color_list = []
+        group_id_list = []
+
+        for group_id, cluster in enumerate(self.clusters):
+            group_id_list_sort.extend([group_id] * len(cluster.cpt_names))
+            cpt_names_list.extend(cluster.cpt_names)
+            color_list.extend(
+                [
+                    "tab:green"
+                    if cluster.spatial_check and cluster.centre_to_centre_check
+                    else "tab:orange"
+                    if cluster.spatial_check and ~cluster.centre_to_centre_check
+                    else "tab:red"
+                ]
+                * len(cluster.cpt_names)
+            )
+            valid_pile_level = np.array(cluster.data.pile_tip_level)[
+                np.array(cluster.data.net_design_bearing_capacity)
+                <= cluster.pile_load_uls
+            ]
+            axes[1].scatter([group_id] * len(valid_pile_level), valid_pile_level)
+            group_id_list.append(group_id)
+
+            data = pd.DataFrame(
+                {
+                    "group_id": group_id_list_sort,
+                    "cpt_names": cpt_names_list,
+                    "colors": color_list,
+                }
+            ).sort_values("cpt_names", ascending=False, key=natsort.natsort_keygen())
+            axes[0].scatter(x="group_id", y="cpt_names", color="colors", data=data)
+            axes[0].set_xlabel("Group ID")
+            axes[0].set_xticks(group_id_list)
+            axes[0].set_ylabel("CPT name")
+            axes[0].grid(which="major", axis="both", alpha=0.1)
+
+        axes[1].set_xlabel("Group ID")
+        axes[1].set_ylabel("Depth [m NAP]")
+        axes[1].grid(which="major", axis="both", alpha=0.1)
+
+        return figure
