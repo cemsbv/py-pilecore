15cc1414a82040ca2ca3f57cb392bfa4477251bf
Robin Wimmers <r.wimmers@cemsbv.io>
2023-10-23 17:14:13 +0200

    feat: add __version__ to package

---
+++ src/pypilecore/_version.py
@@ -0,0 +1,7 @@
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("py-pilecore")
+# during CI
+except PackageNotFoundError:
+    __version__ = "unknown"
