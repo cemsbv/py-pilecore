7adfca768790be4b649f21fb78243fce27b2b5b1
Robin Wimmers <r.wimmers@cemsbv.io>
2023-10-23 10:26:43 +0200

    feat(grouper): add functions to create grouper payload

--- src/pypilecore/input/__init__.py
+++ src/pypilecore/input/__init__.py
@@ -1,3 +1,4 @@
+from .grouper_properties import create_grouper_payload, create_grouper_report_payload
 from .multi_cpt import create_multi_cpt_payload
 from .pile_properties import create_pile_properties_payload
 from .soil_properties import create_soil_properties_payload
@@ -6,4 +7,6 @@ __all__ = [
     "create_pile_properties_payload",
     "create_soil_properties_payload",
     "create_multi_cpt_payload",
+    "create_grouper_payload",
+    "create_grouper_report_payload",
 ]
