47d4e53c7015c017dfde92b2f45a4113144e34a6
Robin Wimmers <46839685+RDWimmers@users.noreply.github.com>
2023-10-25 15:25:27 +0200

    docs: init pages (#10)
    * feat(grouper): add functions to create grouper payload
    
    * feat(grouper): add GrouperResults dataclass
    
    * test(grouper): add test for GrouperResults dataclass
    
    * chore(deps): update dependencies
    
    * docs: update typos in README
    
    * style: format black and isort
    
    * ci(test): add test job to ci
    
    * fix(typing): add missing typing to function
    
    * refactor(plotting): make axes optional
    
    * test: set source path and omit tests in cov report
    
    * chore(deps): add tqdm and docs dependencies
    
    * ci(docs): docs test and deploy job
    
    * feat: add __version__ to package
    
    * docs(grouper): update docstrings
    
    * docs: add build documentation manual
    
    * feat(grouper): add functions to create grouper payload
    
    * feat(grouper): add GrouperResults dataclass
    
    * test(grouper): add test for GrouperResults dataclass
    
    * chore(deps): update dependencies
    
    * docs: update typos in README
    
    * style: format black and isort
    
    * ci(test): add test job to ci
    
    * fix(typing): add missing typing to function
    
    * refactor(plotting): make axes optional
    
    * test: set source path and omit tests in cov report
    
    * chore(deps): add tqdm and docs dependencies
    
    * ci(docs): docs test and deploy job
    
    * feat: add __version__ to package
    
    * docs(grouper): update docstrings
    
    * docs: add build documentation manual
    
    * style(typing): fix mypy errors
    
    * chore(deps): add tqdm and docs dependencies
    
    * ci(docs): docs test and deploy job
    
    * feat: add __version__ to package
    
    * docs(grouper): update docstrings
    
    * docs: add build documentation manual
    
    * fix: update pyproject.toml
    
    * chore(deps): add tqdm and docs dependencies
    
    * feat: add __version__ to package
    
    * docs(grouper): update docstrings
    
    * docs: add build documentation manual
    
    * fix: update pyproject.toml
    
    * chore(deps): upgrade dependencies

---
+++ docs/conf.py
@@ -0,0 +1,62 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+
+import os
+import sys
+from typing import List
+
+sys.path.insert(0, os.path.abspath("../src"))
+os.environ["DOC_PATH"] = os.path.dirname(__file__)
+
+import pypilecore  # noqa: E402
+
+project = "py-pilecore"
+copyright = "2023, CEMS BV"
+author = "Thijs Lukkezen"
+
+# The full version, including alpha/beta/rc tags
+release = pypilecore.__version__
+
+# 'sphinx.ext.napoleon' Used for numpy docstring support
+extensions = [
+    "IPython.sphinxext.ipython_console_highlighting",
+    "IPython.sphinxext.ipython_directive",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.mathjax",
+    "sphinx_autodoc_typehints",
+    "matplotlib.sphinxext.plot_directive",
+]
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ["_templates"]
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns: List = []
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = "sphinx_rtd_theme"
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ["_static"]
+
+html_css_files = ["css/index.css"]
