c1f90d211c5527a51f03ace7c9d5a8d4ee240ab6
Thijs Lukkezen <t.lukkezen@cemsbv.io>
2023-11-01 15:18:22 +0100

    test: rename test_soil to test_input

--- tests/test_input.py
+++ tests/test_input.py
@@ -3,12 +3,15 @@ from pygef.cpt import CPTData
 from pypilecore.input import create_soil_properties_payload
 
 
-def test_soil_properties(cpt: CPTData, mock_classify_response: dict) -> None:
+def test_create_soil_properties_payload(
+    cpt: CPTData, mock_classify_response: dict
+) -> None:
     """
-    Test parsing and plotting in GrouperResults object
+    Test creating the soil-properties payload with the `create_soil_properties_payload`
+    function.
     """
 
-    # test parsing of response to dataclass
+    # test creating the soil_properties payload
     payload, passover = create_soil_properties_payload(
         cptdata_objects=[cpt],
         classify_tables={cpt.alias: mock_classify_response},
