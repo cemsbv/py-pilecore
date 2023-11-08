1eb6a41325befd0795628f3caaffad6691cfb32d
Thijs Lukkezen <t.lukkezen@cemsbv.io>
2023-11-08 15:29:38 +0100

    test: Add create_multi_cpt_payload unit-tests

--- tests/test_input.py
+++ tests/test_input.py
@@ -7,6 +7,7 @@ from pygef.cpt import CPTData
 
 from pypilecore.input import (
     create_grouper_payload,
+    create_multi_cpt_payload,
     create_pile_properties_payload,
     create_soil_properties_payload,
 )
@@ -340,4 +341,305 @@ def test_create_grouper_payload(
     with pytest.raises(ValueError):
         create_grouper_payload(
             cptgroupresults.cpt_results.cpt_results_dict, pile_load_uls=100
+        )
+
+
+def test_create_multi_cpt_payload_defaults(
+    cpt: CPTData, mock_classify_response: dict
+) -> None:
+    """
+    Test creating the multi_cpt_results payload with the `create_multi_cpt_payload`
+    function with minimal arguments.
+    """
+    payload, _ = create_multi_cpt_payload(
+        pile_tip_levels_nap=[0, 1],
+        cptdata_objects=[cpt],
+        classify_tables={cpt.alias: mock_classify_response},
+        groundwater_level_nap=-2.5,
+        pile_type="concrete",
+        specification="1",
+        installation="A",
+        pile_shape="round",
+        diameter_base=0.3,
+    )
+
+    # Check the payload content
+
+    # We don't completely check the soil_properties list here
+    assert isinstance(payload.pop("list_soil_properties"), list)
+
+    assert payload.pop("excavation_param_t") == 1.0
+    assert payload.pop("pile_head_level_nap") == "surface"
+    assert payload.pop("rel_pile_load") == 0.7
+    assert payload.pop("friction_range_strategy") == "lower_bound"
+    assert np.isclose(payload.pop("gamma_f_nk"), 1.0)
+    assert np.isclose(payload.pop("gamma_r_b"), 1.2)
+    assert np.isclose(payload.pop("gamma_r_s"), 1.2)
+    pile_properties = payload.pop("pile_properties")
+    assert pile_properties.get("type") == "round"
+    pile_props = pile_properties.get("props")
+    assert pile_props.pop("diameter_base") == 0.3
+
+    # Check the pile-type-specification
+    assert pile_props.pop("pile_type_specification") == dict(
+        installation="A", pile_type="concrete", specification="1"
+    )
+    # Check that no extra keys are present in the pile_type_specification
+    assert len(pile_props.keys()) == 0
+
+    assert payload.pop("pile_tip_levels_nap") == [0, 1]
+    assert np.isclose(payload.pop("soil_load"), 0.0)
+    assert payload.pop("use_almere_rules") is False
+    assert payload.pop("stiff_construction") is False
+
+    # Check that no extra keys are present in the payload
+    assert len(payload.keys()) == 0
+
+
+def test_create_multi_cpt_payload_extended_round_pile(
+    cpt: CPTData, mock_classify_response: dict
+) -> None:
+    """
+    Test creating the multi_cpt_results payload with the `create_multi_cpt_payload`
+    function with extended arguments for a round pile.
+    """
+
+    payload, _ = create_multi_cpt_payload(
+        pile_tip_levels_nap=[0, 1],
+        cptdata_objects=[cpt],
+        classify_tables={cpt.alias: mock_classify_response},
+        groundwater_level_nap=-2.5,
+        pile_type="concrete",
+        specification="1",
+        installation="A",
+        pile_shape="round",
+        diameter_base=0.3,
+        friction_range_strategy="manual",
+        stiff_construction=True,
+        cpts_group=[cpt.alias],
+        fixed_negative_friction_range_nap=(0.0, 5.0),
+        fixed_positive_friction_range_nap=(5.0, "ptl"),
+        negative_shaft_friction=0.0,
+        apply_qc3_reduction=True,
+        relative_pile_load=0.8,
+        pile_load_sls=0.0,
+        soil_load_sls=1.0,
+        pile_head_level_nap=5.0,
+        excavation_depth_nap=-2.0,
+        excavation_param_t=0.5,
+        gamma_f_nk=1.2,
+        gamma_r_s=1.5,
+        gamma_r_b=1.4,
+        individual_negative_friction_range_nap={cpt.alias: [0.0, -5.0]},
+        individual_positive_friction_range_nap={cpt.alias: [-5.0, "ptl"]},
+        diameter_shaft=0.25,
+        height_base=0.5,
+        settlement_curve=2,
+        adhesion=0.01,
+        alpha_p=0.01,
+        alpha_s_clay=0.01,
+        alpha_s_sand=0.01,
+        beta_p=0.01,
+        pile_tip_factor_s=0.01,
+        elastic_modulus=0.01,
+        is_auger=True,
+        is_low_vibrating=True,
+        negative_fr_delta_factor=0.01,
+    )
+
+    # Check the payload content
+
+    # We don't completely check the soil_properties list here
+    assert isinstance(payload.pop("list_soil_properties"), list)
+
+    assert payload.pop("excavation_param_t") == 0.5
+    assert payload.pop("pile_head_level_nap") == 5.0
+    assert payload.pop("rel_pile_load") == 0.8
+    assert payload.pop("friction_range_strategy") == "manual"
+    assert np.isclose(payload.pop("gamma_f_nk"), 1.2)
+    assert np.isclose(payload.pop("gamma_r_b"), 1.4)
+    assert np.isclose(payload.pop("gamma_r_s"), 1.5)
+    pile_properties = payload.pop("pile_properties")
+    assert pile_properties.get("type") == "round"
+    pile_props = pile_properties.get("props")
+    assert np.isclose(pile_props.pop("diameter_base"), 0.3)
+    assert np.isclose(pile_props.pop("diameter_shaft"), 0.25)
+    assert np.isclose(pile_props.pop("height_base"), 0.5)
+    assert pile_props.pop("settlement_curve") == 2
+    assert np.isclose(pile_props.pop("adhesion"), 0.01)
+    assert np.isclose(pile_props.pop("alpha_p"), 0.01)
+    assert np.isclose(pile_props.pop("alpha_s_clay"), 0.01)
+    assert np.isclose(pile_props.pop("alpha_s_sand"), 0.01)
+    assert np.isclose(pile_props.pop("beta_p"), 0.01)
+    assert np.isclose(pile_props.pop("pile_tip_factor_s"), 0.01)
+    assert np.isclose(pile_props.pop("elastic_modulus"), 0.01)
+    assert pile_props.pop("is_auger") is True
+    assert pile_props.pop("is_low_vibrating") is True
+    assert np.isclose(pile_props.pop("negative_fr_delta_factor"), 0.01)
+
+    # Check the pile-type-specification
+    assert pile_props.pop("pile_type_specification") == dict(
+        installation="A", pile_type="concrete", specification="1"
+    )
+    # Check that no extra keys are present in the pile_type_specification
+    assert len(pile_props.keys()) == 0
+
+    assert payload.pop("pile_tip_levels_nap") == [0, 1]
+    assert np.isclose(payload.pop("soil_load"), 1.0)
+    assert payload.pop("use_almere_rules") is False
+    assert payload.pop("stiff_construction") is True
+
+    # Check the optional payload keys
+    assert payload.pop("cpts_group") == [cpt.alias]
+    assert payload.pop("fixed_negative_friction_range_nap") == (0.0, 5.0)
+    assert payload.pop("fixed_positive_friction_range_nap") == (5.0, "ptl")
+    assert np.isclose(payload.pop("f_nk"), 0.0)
+    assert payload.pop("apply_qc3_reduction") is True
+    assert np.isclose(payload.pop("pile_load"), 0.0)
+    assert np.isclose(payload.pop("excavation_depth_nap"), -2.0)
+
+    # Check that no extra keys are present in the payload
+    assert len(payload.keys()) == 0
+
+
+def test_create_multi_cpt_payload_extended_rect_pile(
+    cpt: CPTData, mock_classify_response: dict
+) -> None:
+    """
+    Test creating the multi_cpt_results payload with the `create_multi_cpt_payload`
+    function with extended arguments for a rect pile.
+    """
+
+    payload, _ = create_multi_cpt_payload(
+        pile_tip_levels_nap=[0, 1],
+        cptdata_objects=[cpt],
+        classify_tables={cpt.alias: mock_classify_response},
+        groundwater_level_nap=-2.5,
+        pile_type="concrete",
+        specification="1",
+        installation="A",
+        pile_shape="rect",
+        width_base_large=0.3,
+        friction_range_strategy="manual",
+        stiff_construction=True,
+        cpts_group=[cpt.alias],
+        fixed_negative_friction_range_nap=(0.0, 5.0),
+        fixed_positive_friction_range_nap=(5.0, "ptl"),
+        negative_shaft_friction=0.0,
+        apply_qc3_reduction=True,
+        relative_pile_load=0.8,
+        pile_load_sls=0.0,
+        soil_load_sls=1.0,
+        pile_head_level_nap=5.0,
+        excavation_depth_nap=-2.0,
+        excavation_param_t=0.5,
+        gamma_f_nk=1.2,
+        gamma_r_s=1.5,
+        gamma_r_b=1.4,
+        individual_negative_friction_range_nap={cpt.alias: [0.0, -5.0]},
+        individual_positive_friction_range_nap={cpt.alias: [-5.0, "ptl"]},
+        width_base_small=0.25,
+        width_shaft_large=0.3,
+        width_shaft_small=0.25,
+        height_base=0.5,
+        settlement_curve=2,
+        adhesion=0.01,
+        alpha_p=0.01,
+        alpha_s_clay=0.01,
+        alpha_s_sand=0.01,
+        beta_p=0.01,
+        pile_tip_factor_s=0.01,
+        elastic_modulus=0.01,
+        is_auger=True,
+        is_low_vibrating=True,
+        negative_fr_delta_factor=0.01,
+    )
+
+    # Check the payload content
+
+    # We don't completely check the soil_properties list here
+    assert isinstance(payload.pop("list_soil_properties"), list)
+
+    assert payload.pop("excavation_param_t") == 0.5
+    assert payload.pop("pile_head_level_nap") == 5.0
+    assert payload.pop("rel_pile_load") == 0.8
+    assert payload.pop("friction_range_strategy") == "manual"
+    assert np.isclose(payload.pop("gamma_f_nk"), 1.2)
+    assert np.isclose(payload.pop("gamma_r_b"), 1.4)
+    assert np.isclose(payload.pop("gamma_r_s"), 1.5)
+    pile_properties = payload.pop("pile_properties")
+    assert pile_properties.get("type") == "rect"
+    pile_props = pile_properties.get("props")
+    assert np.isclose(pile_props.pop("width_base_large"), 0.3)
+    assert np.isclose(pile_props.pop("width_base_small"), 0.25)
+    assert np.isclose(pile_props.pop("width_shaft_large"), 0.3)
+    assert np.isclose(pile_props.pop("width_shaft_small"), 0.25)
+    assert np.isclose(pile_props.pop("height_base"), 0.5)
+    assert pile_props.pop("settlement_curve") == 2
+    assert np.isclose(pile_props.pop("adhesion"), 0.01)
+    assert np.isclose(pile_props.pop("alpha_p"), 0.01)
+    assert np.isclose(pile_props.pop("alpha_s_clay"), 0.01)
+    assert np.isclose(pile_props.pop("alpha_s_sand"), 0.01)
+    assert np.isclose(pile_props.pop("beta_p"), 0.01)
+    assert np.isclose(pile_props.pop("pile_tip_factor_s"), 0.01)
+    assert np.isclose(pile_props.pop("elastic_modulus"), 0.01)
+    assert pile_props.pop("is_auger") is True
+    assert pile_props.pop("is_low_vibrating") is True
+    assert np.isclose(pile_props.pop("negative_fr_delta_factor"), 0.01)
+
+    # Check the pile-type-specification
+    assert pile_props.pop("pile_type_specification") == dict(
+        installation="A", pile_type="concrete", specification="1"
+    )
+    # Check that no extra keys are present in the pile_type_specification
+    assert len(pile_props.keys()) == 0
+
+    assert payload.pop("pile_tip_levels_nap") == [0, 1]
+    assert np.isclose(payload.pop("soil_load"), 1.0)
+    assert payload.pop("use_almere_rules") is False
+    assert payload.pop("stiff_construction") is True
+
+    # Check the optional payload keys
+    assert payload.pop("cpts_group") == [cpt.alias]
+    assert payload.pop("fixed_negative_friction_range_nap") == (0.0, 5.0)
+    assert payload.pop("fixed_positive_friction_range_nap") == (5.0, "ptl")
+    assert np.isclose(payload.pop("f_nk"), 0.0)
+    assert payload.pop("apply_qc3_reduction") is True
+    assert np.isclose(payload.pop("pile_load"), 0.0)
+    assert np.isclose(payload.pop("excavation_depth_nap"), -2.0)
+
+    # Check that no extra keys are present in the payload
+    assert len(payload.keys()) == 0
+
+
+def test_create_multi_cpt_payload_errors(
+    cpt: CPTData, mock_classify_response: dict
+) -> None:
+    """
+    Test creating the multi_cpt_results payload with the `create_multi_cpt_payload`
+    with erroneous input.
+    """
+
+    with pytest.raises(ValueError):
+        create_multi_cpt_payload(
+            pile_tip_levels_nap=[0, 1],
+            cptdata_objects=[cpt],
+            classify_tables={cpt.alias: mock_classify_response},
+            groundwater_level_nap=-2.5,
+            pile_type="A",
+            specification="concrete",
+            installation="1",
+            pile_shape="round",
+        )
+
+    with pytest.raises(ValueError):
+        create_multi_cpt_payload(
+            pile_tip_levels_nap=[0, 1],
+            cptdata_objects=[cpt],
+            classify_tables={cpt.alias: mock_classify_response},
+            groundwater_level_nap=-2.5,
+            pile_type="A",
+            specification="concrete",
+            installation="1",
+            pile_shape="rect",
         )
