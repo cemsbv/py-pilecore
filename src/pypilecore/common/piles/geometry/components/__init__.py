39913aa3a65120e4cdadb0d21b127bd0c8da8437
Thijs Lukkezen <t.lukkezen@cemsbv.io>
2024-07-18 12:40:30 +0200

    chore: Import pile objects in parent modules

--- src/pypilecore/common/piles/geometry/components/__init__.py
+++ src/pypilecore/common/piles/geometry/components/__init__.py
@@ -1,8 +1,9 @@
-from .common import PrimaryPileComponentDimension
+from .common import PrimaryPileComponentDimension, _BasePileGeometryComponent
 from .rectangle import RectPileGeometryComponent
 from .round import RoundPileGeometryComponent
 
 __all__ = [
+    "_BasePileGeometryComponent",
     "PrimaryPileComponentDimension",
     "RectPileGeometryComponent",
     "RoundPileGeometryComponent",
