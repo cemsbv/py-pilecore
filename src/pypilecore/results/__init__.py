6ae7207eb4ff61019f6913ecc2e9ca09325fe1cb
Pablo Vasconez <p.vasconez@cemsbv.io>
2024-09-11 17:00:09 +0200

    chore(cases_multicpt): add CasesMultiCPTBearingResults to the init of the results module

--- src/pypilecore/results/__init__.py
+++ src/pypilecore/results/__init__.py
@@ -3,6 +3,8 @@ from pypilecore.results.grouper_result import GrouperResults
 from pypilecore.results.multi_cpt_results import MultiCPTBearingResults
 from pypilecore.results.single_cpt_results import SingleCPTBearingResults
 from pypilecore.results.soil_properties import SoilProperties
+from pypilecore.results.cases_multi_cpt_results import CasesMultiCPTBearingResults
+
 
 __all__ = [
     "CasesMultiCPTBearingResults",
