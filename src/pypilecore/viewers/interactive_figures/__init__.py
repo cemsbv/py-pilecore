1e47147940c48abc1b3542d4d3c181b161980842
Pablo Vasconez <p.vasconez@cemsbv.io>
2024-09-13 08:53:07 +0200

    feat(viewer_results): add the results viewer ViewerCptGroupResults

--- src/pypilecore/viewers/interactive_figures/__init__.py
+++ src/pypilecore/viewers/interactive_figures/__init__.py
@@ -1,7 +1,11 @@
+from pypilecore.viewers.interactive_figures.figure_cpt_group_results_versus_ptls import (
+    FigureCPTGroupResultsVersusPtls,
+)
 from pypilecore.viewers.interactive_figures.figure_cpt_results_versus_ptls import (
     FigureCPTResultsVersusPtls,
 )
 
 __all__ = [
     "FigureCPTResultsVersusPtls",
+    "FigureCPTGroupResultsVersusPtls",
 ]
