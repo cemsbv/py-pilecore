de63b9b6a5d7cf63f4a4ef90f15a5d9825475545
Pablo Vasconez <p.vasconez@cemsbv.io>
2024-10-01 13:07:53 +0200

    fix(viewer_results): fix error of not finding color for NaN results for ViewerCptResultsPlanView

--- src/pypilecore/viewers/interactive_figures/utils.py
+++ src/pypilecore/viewers/interactive_figures/utils.py
@@ -1,5 +1,6 @@
 from typing import Any
 
+import numpy as np
 import plotly
 
 
@@ -19,14 +20,14 @@ def get_continuous_color(colorscale: Any, intermed: float) -> Any:
         colorscale = plotly.colors.make_colorscale(viridis_colors, scale=scale)
 
     :param colorscale: A plotly continuous colorscale defined with RGB string colors.
-    :param intermed: value in the range [0, 1]
+    :param intermed: value in the range [0, 1]. If `intermed` is NaN, it returns the first color in the colorscale.
     :return: color in rgb string format
     :rtype: str
     """
     if len(colorscale) < 1:
         raise ValueError("colorscale must have at least one color")
 
-    if intermed <= 0 or len(colorscale) == 1:
+    if intermed <= 0 or len(colorscale) == 1 or np.isnan(intermed):
         return colorscale[0][1]
     if intermed >= 1:
         return colorscale[-1][1]
