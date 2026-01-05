78a1151ce48ea9ad330145e5060343ad1574aa4e
Thijs Lukkezen <t.lukkezen@cemsbv.io>
2026-01-05 14:26:29 +0100

    feat: Refactor ViewerCptResults and FigureCPTResultsVersusPtls to also handle Grouper results. Removes ViewerCptGroupResults

--- tests/viewers/figures/test_figure_cpt_results_versus_ptls.py
+++ tests/viewers/figures/test_figure_cpt_results_versus_ptls.py
@@ -17,7 +17,7 @@ def test_init_valid_input(
     cases_multi_results = mock_cases_multi_cpt_bearing_results
 
     figure = FigureCPTResultsVersusPtls(
-        cases_multi_results=cases_multi_results,
+        results_cases=cases_multi_results,
     )
 
     assert figure.results == cases_multi_results
@@ -33,7 +33,7 @@ def test_init_invalid_input() -> None:
     returns the expected errors.
     """
     with pytest.raises(TypeError, match="'cases_multi_results'"):
-        FigureCPTResultsVersusPtls(cases_multi_results="invalid")
+        FigureCPTResultsVersusPtls(results_cases="invalid")
 
 
 def test_get_visible_test_ids(
@@ -45,7 +45,7 @@ def test_get_visible_test_ids(
     cases_multi_results = mock_cases_multi_cpt_bearing_results
 
     figure = FigureCPTResultsVersusPtls(
-        cases_multi_results=cases_multi_results,
+        results_cases=cases_multi_results,
     )
 
     # Test that initially no test_ids are visible
@@ -66,7 +66,7 @@ def test_show_case_and_result(
     cases_multi_results = mock_cases_multi_cpt_bearing_results
 
     figure = FigureCPTResultsVersusPtls(
-        cases_multi_results=cases_multi_results,
+        results_cases=cases_multi_results,
     )
 
     # Invalid case
