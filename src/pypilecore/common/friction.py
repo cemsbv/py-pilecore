85a5396fb1cdbe1552766a3fa8449d26951f58f8
Ivaylo Pantev <i.pantev@cemsbv.io>
2026-01-08 14:25:19 +0200

    feat(api): improve FrictionRangeStrategy validation

--- src/pypilecore/common/friction.py
+++ src/pypilecore/common/friction.py
@@ -1,4 +1,5 @@
 from enum import StrEnum
+from typing import List
 
 class FrictionRangeStrategy(StrEnum):
     lower_bound = "lower_bound"
@@ -15,10 +16,12 @@ class SchemaKeys(StrEnum):
 
 class FrictionSettings:
     def __init__(self, 
-                 friction_range_strategy = FrictionRangeStrategy.lower_bound, 
-                 negative_friction = None, 
-                 negative_friction_range_nap = None, 
-                 positive_friction_range_nap = None):
+                 friction_range_strategy: str | FrictionRangeStrategy = FrictionRangeStrategy.lower_bound, 
+                 negative_friction: float | None = None, 
+                 negative_friction_range_nap: List[float] | None = None, 
+                 positive_friction_range_nap: List[float] | None = None):
+        # With loop for compatibility in python 3.11
+        assert any(item.value == friction_range_strategy for item in FrictionRangeStrategy), f"Invalid value for friction_range_strategy defined, please enter one of {[item.value for item in FrictionRangeStrategy]}"
         self.friction_range_strategy = friction_range_strategy
         self.negative_friction = negative_friction
         self.negative_friction_range_nap = negative_friction_range_nap
